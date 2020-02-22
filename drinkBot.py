aa80385e13501a8e986fc202493e0ec356b5415c
klinstifen <klinstifen@gmail.com>
2020-02-22 15:50:04 -0500

    Added comments

--- drinkBot.py
+++ drinkBot.py
@@ -10,37 +10,39 @@ button4 = Button(24)
 buttonDelay = 0.2 #debounce
 
 pumpTime = 3 * 1000 #amount of time pump automatically runs
-pumpList = {1:0,2:0,3:0,4:0} #pumpTimers
+pumpTimers = {1:0,2:0,3:0,4:0} #pumpTimers
 mh = Adafruit_MotorHAT(addr=0X60)
 
 def runPump(id):
-	global pumpList
+	global pumpTimers
 	mid = int((id+1)/2) #set motorID
 	motor = mh.getMotor(mid)
 	motor.setSpeed(255)
-	print("running pump: ",id)
+	print("Running pump: ",id)
 	if id %2:
 		motor.run(Adafruit_MotorHAT.FORWARD);
 	else:
 		motor.run(Adafruit_MotorHAT.BACKWARD);
-	pumpList[id] = getTime()
+	pumpTimers[id] = getTime()
 	time.sleep(buttonDelay)
 
 def checkPumpTimers():
-	global pumpList
-	for p in pumpList:
-		if(pumpList[p]):
-			if((getTime() - pumpList[p]) > pumpTime):
-				print("stopping pump:",p)
+	global pumpTimers
+	for p in pumpTimers:
+		if(pumpTimers[p]):
+			if((getTime() - pumpTimers[p]) > pumpTime):
 				stopPump(p)
-				pumpList[p] = 0
+				pumpTimers[p] = 0
 
 def stopPump(id):
+	print("Stopping pump: ")
 	mh.getMotor(int((id+1)/2)).run(Adafruit_MotorHAT.RELEASE);
 
 def getTime():
 	return int(round(time.time() * 1000))
 
+print("Beginning drinkBot.py")
+print("Auto-pump timer set to ",pumpTime," second(s)")
 while True:
 	if button1.is_pressed:
 		runPump(1)
